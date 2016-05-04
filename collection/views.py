506fb2727be252c2c9d7656a44bb6a4a539c5012
JLM <judashlie@gmail.com>
2016-05-04 14:15:38 -0700

    second commit

--- collection/views.py
+++ collection/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	#this is the new view
-	return render(request, 'index.html')
+	# this is the new view
+	# defining the variable
+	number = 6
+	thing = "Thing Name"
+	# passing the variable to the view
+	return render(request, 'index.html', {
+		'number': number,
+		'thing': thing,
+		})
